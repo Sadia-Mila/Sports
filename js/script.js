8b700218cd5e518c4e4c8a896d4dbf1c764fe5b1
Sadia Haque <sadiahaque2017@gmail.com>
2025-03-24 10:04:53 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,17 @@
+
+// Counter Up js Start
+
+$(document).ready(function () {
+    $('.counter').counterUp({
+      delay: 10,
+      time: 1000
+    });
+  });
+// Counter Up js End
+
+// Mix it Up js start
+
+var containerEl = document.querySelector('.filterMixItUp');
+
+ var mixer = mixitup(containerEl);
+// Mix it Up js End
